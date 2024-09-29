40620de839f628b206ae3f62b154c3d6ea17dc86
Comic Solvency <131397277+ComicBurduli@users.noreply.github.com>
2024-09-29 18:23:05 +0400

    Add files via upload

---
+++ 2024-2025 first homework exeptions/ConsoleApp2/InvalidUsernameException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class InvalidUsernameException : Exception
+    {
+
+        public InvalidUsernameException(string massage) : base(massage) 
+        
+        { 
+        
+        
+        
+        }   
+    }
+}
---
+++ 2024-2025 first homework exeptions/ConsoleApp2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+            try
+            {
+                Console.WriteLine("Enter Your UserName : ");
+                string username = Console.ReadLine();
+
+                if (username.Contains(" "))
+                    throw new InvalidUsernameException("Use Username without space !!!");
+
+
+                Console.WriteLine("Enter your password : ");
+                string password = Console.ReadLine();
+
+                if (password.Length < 6)
+                {
+                    throw new InvalidPasswordException("your password is not strong ");
+
+                }
+
+            }
+         
+        catch ( InvalidPasswordException exception)
+            {
+                Console.WriteLine($"Erorr : {exception.Message}");   
+            }
+
+            catch (InvalidUsernameException exeption)
+            {
+                Console.WriteLine($"error : {exeption.Message}");    
+            } 
+
+
+            //catch (Exception exeption)
+            //{
+            //    Console.WriteLine($"aramosalodneli erori {exeption.Message}");
+            //}
+            
+
+        }
+    }
+}
